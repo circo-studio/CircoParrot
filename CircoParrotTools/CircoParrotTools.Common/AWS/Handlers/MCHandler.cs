f744aad9c5b8a3d076e26c8db69fc058c18c46a3
FacundoBistolfi <fbistolfi@circostudio.com>
2018-07-25 11:34:50 -0300

    WIP

--- CircoParrotTools/CircoParrotTools.Common/AWS/Handlers/MCHandler.cs
+++ CircoParrotTools/CircoParrotTools.Common/AWS/Handlers/MCHandler.cs
@@ -2,25 +2,49 @@
 using Amazon.MediaConvert.Model;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
 using System.Threading.Tasks;
-
 
 namespace CircoParrotTools.Common.AWS.Handlers
 {
     class MCHandler
     {
-        
-
-        public async Task<List<Job>> listObjects(String bucket)
+        public async Task<List<Job>> ListJobs()
         {
-            AmazonMediaConvertClient client = new AmazonMediaConvertClient("AKIAIYIYEIUSITOE3FUQ", "2bS3qTyjxyves/zeqR+gmiERhxGvU1fv2xWvCjrg", Amazon.RegionEndpoint.USEast1);
+            String mediaConvertEndpoint = "";
+            if (String.IsNullOrEmpty(mediaConvertEndpoint))
+            {
+                // Obtain the customer-specific MediaConvert endpoint
+                AmazonMediaConvertClient client = new AmazonMediaConvertClient(Amazon.RegionEndpoint.USEast1);
+                DescribeEndpointsRequest describeRequest = new DescribeEndpointsRequest();
+                DescribeEndpointsResponse describeResponse = await client.DescribeEndpointsAsync(describeRequest);
+                mediaConvertEndpoint = describeResponse.Endpoints[0].Url;
+            }
+            AmazonMediaConvertConfig mcConfig = new AmazonMediaConvertConfig
+            {
+                ServiceURL = mediaConvertEndpoint
+            };
+            String key = "";
+            String secret = "";
+            try
+            {
+                using (StreamReader sr = new StreamReader("c:\\temp\awskey.txt"))
+                {
+                    key = sr.ReadLine();
+                    secret = sr.ReadLine();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Could not read the file");
+            }
+
+
+            AmazonMediaConvertClient mcClient = new AmazonMediaConvertClient(key, secret, mcConfig);
             ListJobsRequest req = new ListJobsRequest();
-            ListJobsResponse listJobs = await client.ListJobsAsync(req);
+            ListJobsResponse listJobs = await mcClient.ListJobsAsync(req);
             return listJobs.Jobs;
         }
-
-
-
     }
 }
